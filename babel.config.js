15cfa62cf426fb5ba9d846d1d52e4b8114ec339d
Maicon Gilton <maicongilton@gmail.com>
2023-10-14 18:55:33 -0300

    chore: add custom alias for directories

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,16 @@ module.exports = {
     '@babel/preset-typescript'
   ],
   plugins: [
+    ['module-resolver', {
+      alias: {
+        '@modules': './api/src/modules',
+        '@config': './api/src/config',
+        '@utils': './api/src/utils',
+        '@services': './api/src/services',
+        '@middlewares': './api/src/middlewares',
+        '@providers': './api/src/providers'
+      }
+    }]
   ],
   ignore: [
     '**/*.spec.ts',
